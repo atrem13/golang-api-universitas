8b7e8b22f93bdbe3d41c2cf4a4e8c5d30a2668e8
atrem13 <mertayoga17.my@gmail.com>
2022-11-08 16:36:34 +0800

    update

--- prodi/entity.go
+++ prodi/entity.go
@@ -1,1 +1,10 @@
 package prodi
+
+import "time"
+
+type Prodi struct {
+	ID        int
+	Nama      string
+	Created   time.Time
+	UpdatedAt time.Time
+}
--- prodi/input.go
+++ prodi/input.go
@@ -1,1 +1,9 @@
 package prodi
+
+type GetProdiDetailInput struct {
+	ID int `uri:"id" binding:"required"`
+}
+
+type CreateProdiInput struct {
+	Nama string `json:"name" binding:"required"`
+}
