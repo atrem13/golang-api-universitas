8152cadeacc0e71ea15007b341fcbbca18866893
atrem13 <mertayoga17.my@gmail.com>
2022-11-09 15:00:06 +0800

    update

--- mahasiswa/repository.go
+++ mahasiswa/repository.go
@@ -1,1 +1,37 @@
 package mahasiswa
+
+import "github.com/jinzhu/gorm"
+
+type Repository interface {
+	FindAll() ([]Mahasiswa, error)
+	FindByID(ID int) (Mahasiswa, error)
+	Create(mahasisw Mahasiswa) (Mahasiswa, error)
+	Update(mahasiswa Mahasiswa) (Mahasiswa, error)
+	Delete(mahasiswa Mahasiswa) (Mahasiswa, error)
+}
+
+type repository struct {
+	db *gorm.DB
+}
+
+func NewRepository(db *gorm.DB) *repository {
+	return &repository{db: db}
+}
+
+func (r *repository) FindAll() ([]Mahasiswa, error) {
+	var mahasiswas []Mahasiswa
+	err := r.db.Preload("Prodi").Find(&mahasiswas).Error
+	if err != nil {
+		return mahasiswas, err
+	}
+	return mahasiswas, nil
+}
+
+func (r *repository) FindByID(ID int) (Mahasiswa, error) {
+	var mahasiswa Mahasiswa
+	err := r.db.Where("id = ?", ID).Preload("Prodi").Find(&mahasiswa).Error
+	if err != nil {
+		return mahasiswa, err
+	}
+	return mahasiswa, nil
+}
